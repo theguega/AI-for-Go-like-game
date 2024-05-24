b4f24ce09e0bfc2f4ea00094158d1fd8f7c9e0f3
Theo <theo.guegan@etu.utc.fr>
2024-05-24 17:57:15 +0200

    add alpha beta algorythm

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     profiler.enable()
 
     # ---- Boucle de jeu ----
-    name = "Gopher"
+    name = "Dodo"
     size = 5
-    nb_iteration = 100
+    nb_iteration = 1
     victoire_rouge = 0
     victoire_bleu = 0
 
@@ -22,9 +22,9 @@ if __name__ == "__main__":
         env = gopher_dodo.initialize(name, initial_state, gopher_dodo.R, size, 50)
         while not env.final():
             if env.player == gopher_dodo.R:
-                action = env.strategy_random()
-            else:
                 action = env.strategy_alpha_beta()
+            else:
+                action = env.strategy_random()
             if action is not None:
                 env.play(action)
         
